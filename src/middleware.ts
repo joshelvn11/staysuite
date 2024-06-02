9c2e047bab266b35452929da3ab4c4f9ae257f6b
Josh Beaver <josh.elvn11@gmail.com>
2024-06-02 21:53:39 +0100

    [FEAT] created basic domain mapping for testing purposes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { hostname, pathname } = req.nextUrl;
-  //const clientName = getClientNameFromHostname(hostname);
+  const clientName = getClientNameFromHostname(hostname);
 
   if (pathname.startsWith("/_next/static/")) {
     return NextResponse.next();
   }
 
-  const url = req.nextUrl.clone();
-  console.log(url.pathname);
-  url.pathname = `/john${pathname}`;
-  return NextResponse.rewrite(url);
+  if (clientName) {
+    const url = req.nextUrl.clone();
+    console.log(url.pathname);
+    url.pathname = `/${clientName}${pathname}`;
+    return NextResponse.rewrite(url);
+  }
 
   return NextResponse.next();
 }
 
-// function getClientNameFromHostname(hostname: string): string | null {
-//   const mappings = {
-//     "john.com": "john",
-//     "emily.com": "emily",
-//   };
-//   return mappings[hostname] || null;
-// }
+function getClientNameFromHostname(hostname: string): string | null {
+  const mappings: { [key: string]: string } = {
+    "elandsbaystays.com": "Elands Bay",
+    "john.com": "john",
+    localhost: "test",
+  };
+  return mappings[hostname] || null;
+}
