de1c0c06d1e814e77ec2552569742810fd89404c
Josh Beaver <josh.elvn11@gmail.com>
2024-06-07 22:58:24 +0100

    [FEAT] added view accommodation button and adjusted accommodation card height

--- src/components/AccommodationCard.tsx
+++ src/components/AccommodationCard.tsx
@@ -7,7 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { getCurrencySymbol } from "@/utils/currencyUtils";
+import Link from "next/link";
 
 interface AccommodationCardProps {
   key: number;
@@ -28,7 +30,7 @@ const AccommodationCard: React.FC<AccommodationCardProps> = (props) => {
   }
 
   return (
-    <Card>
+    <Card className="flex flex-col">
       <img
         className="rounded-t-xl border max-h-52 w-full object-cover"
         src="https://a0.muscache.com/im/pictures/miso/Hosting-53452882/original/ab01d765-00ef-482c-97b7-b16aa3c25973.jpeg?im_w=1200"
@@ -36,15 +38,18 @@ const AccommodationCard: React.FC<AccommodationCardProps> = (props) => {
       <CardHeader>
         <CardTitle>{props.name}</CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex grow">
         <p>{props.excerpt}</p>
       </CardContent>
       <CardFooter>
-        <div>
+        <div className="flex justify-between w-full">
           <p className="text-lg font-semibold">
             {getCurrencySymbol(props.currency)}
             {props.price} {priceTypeText}
           </p>
+          <Button asChild>
+            <Link href="">View</Link>
+          </Button>
         </div>
       </CardFooter>
     </Card>
