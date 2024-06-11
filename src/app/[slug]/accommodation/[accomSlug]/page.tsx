76834e0d4dc2ac7a728660117efce66e1389c673
Josh Beaver <josh.elvn11@gmail.com>
2024-06-11 13:29:42 +0100

    [FEAT] created base accommodation detail page

---
+++ src/app/[slug]/accommodation/[accomSlug]/page.tsx
@@ -0,0 +1,30 @@
+import { RootContainer, Header, PageContent } from "@/components";
+import { getSiteData } from "@/api/siteService";
+import { getAccommodationDetail } from "@/api/accommodationService";
+import { getPriceTypeText } from "@/utils/currencyUtils";
+
+export default async function AccommodationDetail({
+  params,
+}: {
+  params: { slug: string; accomSlug: String };
+}) {
+  // Get site detail data from server
+  const siteData = await getSiteData(params.slug);
+  // Get accommodation detail data from server
+  const accomData = await getAccommodationDetail(params.accomSlug);
+
+  return (
+    <RootContainer>
+      <Header title={siteData.site_name} />
+      <PageContent>
+        <div className="flex justify-between w-full">
+          <h2>{accomData.accommodation_name}</h2>
+          <p>
+            {accomData.accomodation_price}{" "}
+            {getPriceTypeText(accomData.accommodation_price_type)}
+          </p>
+        </div>
+      </PageContent>
+    </RootContainer>
+  );
+}
